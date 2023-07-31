d2f1108b96c33defd8f8f387ac1ec986fb391c55
gabriela-mn <cris_1997cm@hotmail.com>
2023-07-31 01:38:01 -0500

    Update webpack.config.js

--- Lab06_SignUpPage-Automatizador/webpack.config.js
+++ Lab06_SignUpPage-Automatizador/webpack.config.js
@@ -1,14 +1,30 @@
 const path = require('path');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   // The entry point file described above
   entry: './src/index.js',
   // The location of the build folder described above
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  // Optional and for development only. This provides the ability to
-  // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+  
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer/"),
+    },
+  },
+  mode: 'development',
+  stats: "minimal", // Muestra solo la información esencial
+  plugins: [
+    /*new Dotenv({
+      path: path.join(__dirname, "./.env"),
+    }),*/
+    new Dotenv()
+  ],
 };
