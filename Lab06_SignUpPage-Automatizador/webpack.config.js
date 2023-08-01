5b8da9a28b86d1a8ab1b8ef495f20fd293d4f8ad
gabriela-mn <cris_1997cm@hotmail.com>
2023-07-31 23:45:01 -0500

    Update webpack.config.js

--- Lab06_SignUpPage-Automatizador/webpack.config.js
+++ Lab06_SignUpPage-Automatizador/webpack.config.js
@@ -1,30 +1,22 @@
-const path = require('path');
+const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   // The entry point file described above
-  entry: './src/index.js',
+  entry: "./src/index.js",
   // The location of the build folder described above
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  
   resolve: {
     fallback: {
-      path: require.resolve("path-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      buffer: require.resolve("buffer/"),
+      path: false,
     },
   },
-  mode: 'development',
-  stats: "minimal", // Muestra solo la información esencial
+  mode: "development",
+  //stats: "minimal", // Muestra solo la información esencial
   plugins: [
-    /*new Dotenv({
-      path: path.join(__dirname, "./.env"),
-    }),*/
-    new Dotenv()
+    new Dotenv(),
   ],
 };
